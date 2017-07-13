057991547005a767188539c4568f746c58fe44e5
Tom Kay <oridan82@gmail.com>
2017-07-13 16:49:01 +0100

    set git ssh command using config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM tomkay/cloud-builder-dep
 
 COPY ssh.sh /ssh.sh
-ENV GIT_SSH=/ssh.sh
 
 COPY lucid-dep.bash /builder/bin/
 ENTRYPOINT ["/builder/bin/lucid-dep.bash"]
--- lucid-dep.bash
+++ lucid-dep.bash
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-KEYS_DIR=/workspace/github-keys
-
-if [ -d $KEYS_DIR ]
-then
-    cd $KEYS_DIR
-    for KEY in *
-    do
-        if [[ ! $KEY =~ \.pub$ ]]
-        then
-            REPO="$(echo "$KEY" | sed 's:_:/:g')"
-            if [ "$REPO" != "" ]
-            then
-                git config --global url."git@github.com:$REPO".insteadOf "https://github.com/$REPO"
-            fi
-        fi
-    done
-
-    cd - >/dev/null
-fi
-
-#echo
-#echo "Generated git config:"
-#cat ~/.gitconfig
-#echo
+git config --global core.sshCommand '/ssh.sh'
 
 exec dep.bash "$@"
