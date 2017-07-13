f6d366f49100f81ae70d7ad39d1bb458c981eb50
Tom Kay <oridan82@gmail.com>
2017-07-13 18:26:38 +0100

    remove condition

--- ssh.sh
+++ ssh.sh
@@ -12,11 +12,8 @@ KEY_FILE="$KEYS_DIR/$SHORT_REPO"
 
 #echo "GIT SSH wrapper: Repo $REPO_PATH" >&2
 
-if [ "$SSH_HOST" = "git@github.com" -a -f "$KEY_FILE" ]
-then
-    #echo "GIT SSH Wrapper: Using key $KEY_FILE for repo $REPO_PATH" >&2
-    chmod 0600 $KEY_FILE
-	SSH_OPTS="-o IdentityFile=$KEY_FILE"
-fi
+#echo "GIT SSH Wrapper: Using key $KEY_FILE for repo $REPO_PATH" >&2
+chmod 0600 $KEY_FILE
+SSH_OPTS="-o IdentityFile=$KEY_FILE"
 
 exec ssh $SSH_OPTS -o StrictHostKeyChecking=no "$@"
